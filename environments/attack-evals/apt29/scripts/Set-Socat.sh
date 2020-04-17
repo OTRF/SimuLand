f6280030124cbfcf469ae9e793c435716f4105ed
Roberto Rodriguez <rrodri0622@gmail.com>
2020-04-17 15:50:38 -0400

    Create Set-Socat.sh

---
+++ environments/attack-evals/apt29/scripts/Set-Socat.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+# Author: Roberto Rodriguez (@Cyb3rWard0g)
+# License: GPL-3.0
+
+usage(){
+    echo " "
+    echo "Usage: $0 [option...]" >&2
+    echo
+    echo "   -i     Target IP Address to redirect traffic to"
+    echo "   -h         help menu"
+    echo
+    echo "Examples:"
+    echo " $0 -i 192.168.0.4"
+    exit 1
+}
+
+# ************ Command Options **********************
+while getopts i:h option
+do
+    case "${option}"
+    in
+        i) TARGET_IP=$OPTARG;;
+        h) usage;;
+        \?) usage;;
+        :  ) echo "Missing option argument for -$OPTARG" >&2; exit 1;;
+        *  ) echo "Unimplemented option: -$OPTARG" >&2; exit 1;;
+    esac
+done
+
+if ((OPTIND == 1))
+then
+    echo "No options specified"
+    usage
+fi
+
+#Install dependencies
+apt update -y
+apt install -y socat
+
+# Set up Socat
+socat TCP-LISTEN:443,fork TCP:${TARGET_IP}:443 &
+socat TCP-LISTEN:1234,fork TCP:${TARGET_IP}:1234 &
+socat TCP-LISTEN:8443,fork TCP:${TARGET_IP}:8443 &
